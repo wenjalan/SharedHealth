76724cf732102481a5713f074eb037ac045be93d
Alan Wen <alanjameswen@outlook.com>
2019-06-05 10:33:19 -0700

    Attached SetHealthBonusPerPlayer Command

--- src/wenjalan/SharedHealth.java
+++ src/wenjalan/SharedHealth.java
@@ -2,6 +2,7 @@ package wenjalan;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import wenjalan.commands.SetHealthBonusPerPlayerCommand;
 import wenjalan.listeners.SharedHealthListener;
 
 public class SharedHealth extends JavaPlugin {
@@ -19,11 +20,17 @@ public class SharedHealth extends JavaPlugin {
     public void onEnable() {
         // attach listeners
         Bukkit.getServer().getPluginManager().registerEvents(new SharedHealthListener(), this);
+
+        // attach commands
+        Bukkit.getPluginCommand("sethealthbonusperplayer").setExecutor(new SetHealthBonusPerPlayerCommand());
     }
 
     @Override
     public void onDisable() {
-        super.onDisable();
+        // set everyone's health back to normal
+        max_health = 20.0D;
+        current_health = 20.0D;
+        SharedHealthListener.syncPlayers();
     }
 
 }
